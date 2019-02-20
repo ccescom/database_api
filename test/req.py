e13a563d5cf0a7410902c4036b070e45e8ad5382
Narasimha1997 <yogaimpression@gmail.com>
2019-02-20 12:02:49 +0530

    Added farmer select and retrive

--- test/req.py
+++ test/req.py
@@ -16,5 +16,36 @@ def test_select() :
 
     print(resp.text)
 
-#test_insert()
-test_select()
+def test_insert_supply_line() :
+    resp = requests.post('http://localhost:5000/api/insert', json = json.load(open('insert_line.json')))
+
+    print(resp.text)
+
+def test_insert_farmer() :
+
+    resp = requests.post('http://localhost:5000/api/insert', json = json.load(open('insert_farmer.json')))
+
+    print(resp.text)
+
+LOC = "09846ad4-34d7-1098"
+
+def select_line_from_location() :
+
+    resp = requests.post('http://localhost:5000/api/select/supply_lines', json = {
+        "location_id" :  "cdc1bc86-34d1-1cdc"
+    })
+
+    print(resp.text)
+
+def select_farmer_from_line() :
+
+    resp = requests.post('http://localhost:5000/api/select/farmers', json = {
+        "line_id" : "3f7bead2-34d6-137"
+    }) 
+
+    print(resp.text)
+
+
+
+#test_insert_supply_line()
+select_farmer_from_line()
